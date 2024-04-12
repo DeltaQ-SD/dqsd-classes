7f40cc538c854d4a0ce8fc886cfb2bc9fb38565e
Neil Davies <neil.davies@pnsol.com>
2024-04-12 12:38:53 +0100

    added initial visualisaton api

--- src/DeltaQ/Model/Utilities.hs
+++ src/DeltaQ/Model/Utilities.hs
@@ -5,6 +5,17 @@ where
 
 import DeltaQ.Model.DeltaQ
 
+class (DeltaQ irv) => DeltaQVisualisation irv where
+  -- | Return a sequence of Time and Probability Mass points of a given length
+  --   over the support of the ΔQ. The sequence is monotonitically increasing in
+  --   both Time and Probablity Mass.
+  asDiscreteCDF :: irv -> Int -> [(Time irv, ProbMass irv)]
+  -- | Return a sequence of (Left) Impulse Probablity mass (equivalent to the
+  --   integral of the Heaviside function at that point) or (Right) a sequence
+  --   of Time and Probability Density. The sequence is monotonitcally increasing in Time.
+  asDiscretePDF :: irv -> Int -> [Either (Time irv, ProbMass irv) [(Time irv, ProbMass irv)]]
+
+{-
 class (DeltaQ icdf) => DeltaQSupport icdf where
   type InvProbMass icdf
 
@@ -15,3 +26,4 @@ class (DeltaQ icdf) => DeltaQSupport icdf where
   toDensity :: icdf -> Time icdf -> {- ProbabilityMass -} icdf
 
   -- views for plotting
+-}
